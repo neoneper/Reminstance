7f9b638583b21d29689da4dfb4b589712b73c93a
Julio Pereira <juliocp.next@gmail.com>
2024-06-13 17:02:16 -0300

    BugFix
    Fixed Visual Element in Remember Instance Component

---
+++ Editor/Editors/RememberInstanceEditor.cs
@@ -0,0 +1,45 @@
+using GameCreator.Runtime.Common;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace GameCreator.Editor.Common
+{
+    [CustomEditor(typeof(RememberInstance))]
+    public class RememberInstanceEditor : UnityEditor.Editor
+    {
+        private static readonly Length DEFAULT_MARGIN_TOP = new Length(5, LengthUnit.Pixel);
+        private static readonly Length ERROR_MARGIN = new Length(10, LengthUnit.Pixel);
+
+
+        private VisualElement m_Root;
+        private ErrorMessage m_Error;
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            this.m_Root = new VisualElement
+            {
+                style =
+                {
+                    marginTop = DEFAULT_MARGIN_TOP
+                }
+            };
+
+            SerializedProperty prefabID = this.serializedObject.FindProperty("m_prefabGUID");
+            SerializedProperty memories = this.serializedObject.FindProperty("m_Memories");
+            SerializedProperty saveUniqueID = this.serializedObject.FindProperty("m_SaveUniqueID");
+
+            PropertyField fieldMemories = new PropertyField(memories);
+            PropertyField fieldUniqueID = new PropertyField(saveUniqueID);
+            Label label = new Label($"Prfab GUID: {prefabID.stringValue}");
+
+            this.m_Root.Add(fieldMemories);
+            this.m_Root.Add(fieldUniqueID);
+            this.m_Root.Add(label);
+
+            return this.m_Root;
+        }
+
+      
+    }
+}
